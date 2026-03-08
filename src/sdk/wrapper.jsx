04908e0e6b1d862d498f5a3c6241d3fdb0566b31
Abdul <abdul@kissflow.com>
2026-03-09 00:44:54 +0530

    old forms

--- src/sdk/wrapper.jsx
+++ src/sdk/wrapper.jsx
@@ -1,5 +1,4 @@
-// import KFSDK from "../lib/kfsdk/kfsdk.es"
-import KFSDK from "/Users/sooryakanth/Desktop/Work/Apps/Product/kf-codebase/frontend/lcnc-sdk-js/packages/sdk/dist/kfsdk.es"
+import KFSDK from 'https://unpkg.com/lowcode-client-sdk@latest/dist/kfsdk.es'
 import React, { useState, useEffect } from 'react'
 
 let kf
